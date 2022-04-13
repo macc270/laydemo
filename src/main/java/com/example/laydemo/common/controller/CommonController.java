f83007563138c416c0bb24dbe3e124bca2301667
bob <122620823@qq.com>
2022-04-14 01:04:41 +0800

    4.13号提交

--- src/main/java/com/example/laydemo/common/controller/CommonController.java
+++ src/main/java/com/example/laydemo/common/controller/CommonController.java
@@ -2,14 +2,12 @@ package com.example.laydemo.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CommonController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "hello springboot!";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
--- src/main/java/com/example/laydemo/user/controller/UserController.java
+++ src/main/java/com/example/laydemo/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.laydemo.common.vo.Result;
 import com.example.laydemo.user.entity.User;
 import com.example.laydemo.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +24,17 @@ public class UserController {
     public Object login(User param, HttpSession session) {
         User user = userService.login(param);
         if (user != null) {
-            user.setPassword(null);
-            session.setAttribute("userInfo", user);
-            return Result.success();
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            //参数1是请求密码，参数2是数据库中加密的值
+            boolean matches = passwordEncoder.matches(param.getPassword(), user.getPassword());
+            if (matches) {
+                //登录成功
+                user.setPassword(null);
+                session.setAttribute("userInfo", user);
+                return Result.success();
+            }
         }
+        //登录失败
         return Result.fail("用户名或密码错误！");
     }
 }
