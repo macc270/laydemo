fa9f9958c3ac45a2c60c09367011eac9f2b3fb5c
bob <122620823@qq.com>
2022-04-18 15:58:13 +0800

    修复EmpQuery传参失败问题，子类未添加@Data注解

--- src/main/java/com/example/laydemo/common/vo/EmpQuery.java
+++ src/main/java/com/example/laydemo/common/vo/EmpQuery.java
@@ -1,7 +1,10 @@
 package com.example.laydemo.common.vo;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class EmpQuery extends Page {
     private String name;
     private Date startDate;
