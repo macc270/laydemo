6ee129608cd66830cf461a77ccab4422908d0228
bob <122620823@qq.com>
2022-04-26 23:57:42 +0800

    拦截器配置类增加configuration注解，成功拦截

--- src/main/java/com/example/laydemo/common/config/MyWebConfig.java
+++ src/main/java/com/example/laydemo/common/config/MyWebConfig.java
@@ -2,11 +2,13 @@ package com.example.laydemo.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MyWebConfig implements WebMvcConfigurer {
     @Autowired
     @Qualifier(value = "loginInterceptor")
@@ -19,9 +21,10 @@ public class MyWebConfig implements WebMvcConfigurer {
         registration.addPathPatterns("/**");
         //放行请求
         registration.excludePathPatterns(
-                "/login" ,
-                "/user/login" ,
-                "/user/logout" ,
+                "/login",
+                "/captcha",
+                "/user/login",
+                "/user/logout",
                 "/layui/**",
                 "/lib/**",
                 "/webjars/**",
